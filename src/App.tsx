43badbd07a9531ae271e714af7aa20aa41886b39
Ali N. Abadian <nagshrizali@gmail.com>
2024-03-09 13:52:11 +0330

    1

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './globals.css';
+
+const App = () => {
+  return (
+    <>
+        <h1 className='text-3xl  font-inter font-bold underline'>
+            hello world!
+        </h1>
+    </>
+  )
+}
+
+export default App
