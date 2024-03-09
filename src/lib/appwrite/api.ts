3b83776ca89967692a6f95a20259cf414b185114
Ali N. Abadian <nagshrizali@gmail.com>
2024-03-09 18:00:11 +0330

    TansTack madareto

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -51,3 +51,15 @@ export async function saveUserToDB(user: {
         console.error(error);
     }
 }
+
+export async function signInAccount(user: { email: string, password: string}){
+    try {
+        const session = await account.createSession(
+            user.email,
+            user.password
+        );
+        return session
+    } catch (error) {
+        console.log(error);
+    }
+}
--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -5,7 +5,7 @@ import {
     useQueryClient,
     useInfiniteQuery
 } from '@tanstack/react-query'
-import { createUserAccount } from '../appwrite/api'
+import { createUserAccount, signInAccount } from '../appwrite/api'
 
 export const useCreateUserAccount = () => {
     return useMutation({
@@ -15,6 +15,11 @@ export const useCreateUserAccount = () => {
 
 export const useSignInAccount = () => {
     return useMutation({
-        mutationFn: (user: INewUser) => createUserAccount(user)
+        mutationFn: (
+            user: { 
+                email: string;
+                password: string 
+                }
+        ) => signInAccount(user)
     })
 }
