974c750df84d5cbdeec494f81700bd1b7c013efd
Ali N. Abadian <nagshrizali@gmail.com>
2024-03-09 14:49:45 +0330

    Start 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import './globals.css';
+import { Routes, Route } from 'react-router-dom';
+import SigninForm from './_auth/forms/SigninForm';
+import { Home } from './_root/pages';
+import SignupForm from './_auth/forms/SignupForm';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
 
 const App = () => {
   return (
     <>
-        <h1 className='text-3xl  font-inter font-bold underline'>
-            hello world!
-        </h1>
+        <main className='flex h-screen'>
+            <Routes>
+                {/* public Routes */}
+                <Route element={<AuthLayout />}>
+                    <Route path='/sign-in' element={<SigninForm />}/>
+                    <Route path='/sign-up' element={<SignupForm />}/>
+                </Route>
+                {/* private Routes */}
+                <Route element={<RootLayout />}>
+                    <Route index element={<Home />} />
+                </Route>
+            </Routes>
+        </main>
     </>
   )
 }
---
+++ src/_auth/AuthLayout.tsx
@@ -0,0 +1,23 @@
+import { Outlet, Navigate } from 'react-router-dom'
+
+import React from 'react'
+
+const AuthLayout = () => {
+  const isAuthenticated = false;
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to='/' />
+      ): (
+        <>
+          <section>
+            <Outlet />
+          </section>
+        </>
+      )}
+    </>
+  )
+}
+
+export default AuthLayout
